605e030a485a72c45d046946c2603eaa629b8822
Rogrback <rfzt96@gmail.com>
2022-07-07 09:31:55 -0500

    Component Category created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import { GifExpertApp } from './components/GifExpertApp';
+
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GifExpertApp/>
+      </header>
+    </div>
+  );
+}
+
---
+++ src/components/GifExpertApp.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch'])
+
+  const onAddCategory = () => {
+    setCategories('Valorant')
+    
+  }
+
+
+  return (
+    <>  
+        <h1>GifExpertApp</h1>
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+          {
+            categories.map(category => {
+              return <li key={category} >{category}</li>
+            })
+          }
+        </ol>
+    </>
+  )
+}
